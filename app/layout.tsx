96b26f9f030bca68faaa1d4835eebc0cc945fc0a
Susu-spec <suleimansuwaibat@gmail.com>
2026-03-11 19:41:55 +0100

    set up project

---
+++ app/layout.tsx
@@ -0,0 +1,64 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const satoshi = localFont({
+  src: [
+    {
+      path: "/public/fonts/Satoshi-Light.otf",
+      weight: "350",
+      style: "normal"
+    },
+    {
+      path: "/public/fonts/Satoshi-Black.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/public/fonts/Satoshi-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "/public/fonts/Satoshi-Bold.otf",
+      weight: "700",
+      style: "normal"
+    }
+  ],
+  variable: "--font-satoshi",
+  display: "swap"
+});
+
+export const metadata: Metadata = {
+  title: "Abuja Blazers",
+  description:
+    "Flag football, community, culture. The Blazers are redefining the game in Abuja, on and off the field.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} ${satoshi.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
