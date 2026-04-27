73659ee634ac1083dc14a3873b72ef2685f20cab
Susu-spec <suleimansuwaibat@gmail.com>
2026-04-27 19:43:52 +0100

    chore: update subdomains

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,30 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
-};
+  async redirects() {
+    return [
+      {
+        source: '/shop',
+        destination: 'https://shop.abujablazers.com',
+        permanent: true,
+      },
+      {
+        source: '/shop/:path*',
+        destination: 'https://shop.abujablazers.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/player-market',
+        destination: 'https://players.abujablazers.com',
+        permanent: true,
+      },
+      {
+        source: '/player-market/:path*',
+        destination: 'https://players.abujablazers.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
